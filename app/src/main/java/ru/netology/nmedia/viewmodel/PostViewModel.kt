19ab9a787212c13f339bfa8936cfcc6ec0c77ed2
Igor Repin <repin_iv@vk.com>
2023-08-21 02:54:20 +0300

    hw crud

--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -1,15 +1,46 @@
 package ru.netology.nmedia.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostRepository
 import ru.netology.nmedia.repository.PostRepositoryInMemoryImpl
 
-class PostViewModel: ViewModel() {
+private val empty = Post(
+    id = 0,
+    content = "",
+    author = "",
+    likedByMe = false,
+    published = "",
+    likes = 0,
+    reposts = 0,
+    views = 0
+)
+
+class PostViewModel : ViewModel() {
     private val repository: PostRepository = PostRepositoryInMemoryImpl()
 
     val data = repository.getAll()
+    val edited = MutableLiveData(empty)
+
+    fun edit(post: Post) {
+        edited.value = post
+    }
+
+    fun changeContentAndSave(content: String) {
+        edited.value?.let {
+            val text = content.trim()
+            if (edited.value?.content != text) {
+                repository.save(it.copy(content = text))
+            }
+            edited.value = empty
+        }
+    }
 
     fun like(id: Int) = repository.like(id)
-
     fun repost(id: Int) = repository.repost(id)
+    fun removeById(id: Int) = repository.removeById(id)
+    fun resetEditing() {
+        edited.value = empty
+    }
 }
