5a974fef843ce9107e6373772a8bdd8f7dd2c614
Igor Repin <repin_iv@vk.com>
2024-02-23 22:45:06 +0500

    hw arch 1

--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -1,18 +1,19 @@
 package ru.netology.nmedia.repository
 
+import androidx.paging.PagingData
 import kotlinx.coroutines.flow.Flow
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.model.PhotoModel
 
 interface PostRepository {
-    val data: Flow<List<Post>>
+    val data: Flow<PagingData<Post>>
 
     fun getNewer(id: Long): Flow<Int>
 
     suspend fun getAll(show: Boolean = true)
     suspend fun save(post: Post)
     suspend fun removeById(id: Long)
-    suspend fun likeById(id: Long)
+    suspend fun likeById(post: Post)
     suspend fun updateShow()
     suspend fun saveWithAttachment(post: Post, photoModel: PhotoModel)
 }
